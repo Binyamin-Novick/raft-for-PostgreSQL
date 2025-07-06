3e97df95b1dd37ed2cd56b9aa6ceb04f3b92df19
b3nov <bnovick2@mail.yu.edu>
2025-07-06 19:48:33 -0400

    the gson will eventuly get replaced with soemthing faster forgot to aculy make an instence of the thread

--- src/main/raft/utils/networking/arper.java
+++ src/main/raft/utils/networking/arper.java
@@ -1,4 +1,17 @@
 package raft.utils.networking;
 
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+
 public class arper {
+    InetSocketAddress socketAddress;
+    public arper(InetSocketAddress  socketAddress){
+        this.socketAddress=socketAddress;
+    }
+
+    public void arpG(){
+
+
+    }
+
 }
